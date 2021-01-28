b2001ee3168124d6cf0de2942ab49bf3683cf8c8
YanaNikishina <63909252+YanaNikishina@users.noreply.github.com>
2021-01-28 12:47:39 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+// первое задание
+function move() {
+  $('.square')
+  .animate({
+    marginLeft: "350px",
+    marginTop: "0px"
+  }, 3000)
+  .animate({
+    marginLeft: "0px"
+  }, 3000)
+}
+setInterval(move, 3000);
+
+// второе задание
+function formSub(e) {
+  e.preventDefault();
+  let name = $("#name").val();
+  let browser = $("input[type='radio']:checked").val() || 'не выбрано';
+  let comment = $("#comment").val() || 'пусто';
+
+  alert(`Ваше имя: ${name}\nПользуетесь браузером: ${browser}\nВаш комментарий: ${comment}`);
+  $(".form").trigger('reset');
+}
+$(".form").submit(formSub);
+// третье задание - слайдер
+$('.slider').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 1
+});
